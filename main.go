91aa958baa2c35b832d8cf7dab81c8a82d61625f
Michael Whatcott <mdwhatcott@gmail.com>
2020-09-18 15:30:06 -0600

    Allow reading from file.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -16,23 +17,34 @@ var Version = "dev"
 
 func main() {
 	log.SetFlags(0)
+	path := flag.String("file", "", "The file with simple commands, one per line.")
 	flag.Usage = func() {
 		log.Println("" +
 			"This script accepts individual commands as non-flag arguments " +
-			"and executes each after a confirmation (<ENTER>).",
+			"or in a text file and executes each after a confirmation (<ENTER>).",
 		)
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	if len(flag.Args()) == 0 {
+	commands := flag.Args()
+
+	if len(commands) == 0 && *path != "" {
+		raw, err := ioutil.ReadFile(*path)
+		if err != nil {
+			log.Fatal(err)
+		}
+		commands = strings.Split(string(raw), "\n")
+	}
+
+	if len(commands) == 0 {
 		flag.Usage()
 		log.Fatal("Please provide at least one command to run.")
 	}
 
 	log.Println("step-script @", Version)
 
-	for _, command := range flag.Args() {
+	for _, command := range commands {
 		line := strings.TrimSpace(command)
 		if line == "" || strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
 			continue
@@ -59,5 +71,5 @@ func main() {
 		}
 	}
 
-	log.Printf("Finished running %d commands.", len(flag.Args()))
+	log.Printf("Finished running %d commands.", len(commands))
 }
