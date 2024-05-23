cc818627ea177203a5ce41276d9e4523a99f33d9
Michael Whatcott <mike@smarty.com>
2024-05-23 16:17:20 -0600

    Revert "Read from stdin if flag set."
    This reverts commit d16bfff71948ebddf25933878ac90c64787799ca.
    
    We can't both read input from stdin and ask the user to hit enter, which
    effectively reads from stdin.

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -16,8 +16,6 @@ var Version = "dev"
 func main() {
 	log.SetFlags(0)
 
-	stdin := false
-	flag.BoolVar(&stdin, "stdin", stdin, "When set, read from stdin first, then any files named by non-flag args.")
 	flag.Usage = func() {
 		log.Println("" +
 			"This script accepts file paths in non-flag args which contain commands that will be executed (w/ bash -c '...'), one at a time, each after a prompt.",
@@ -28,55 +26,41 @@ func main() {
 
 	log.Println("step-script @", Version)
 
-	if stdin {
-		commandsExecuted := executeSteps(os.Stdin)
-		log.Printf("Finished running %d commands from stdin.", commandsExecuted)
-	}
-
 	paths := flag.Args()
-	if !stdin && len(paths) == 0 {
-		log.Fatalln("At least one path is required when not reading from stdin.")
+
+	if len(paths) == 0 {
+		log.Fatalln("At least one path is required.")
 	}
 
+	commandsExecuted := 0
 	for _, path := range paths {
-		file, err := os.Open(path)
+		content, err := os.ReadFile(path)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		commandsExecuted := executeSteps(file)
-		_ = file.Close()
-		log.Printf("Finished running %d commands in %s.", commandsExecuted, path)
-	}
-}
+		scanner := bufio.NewScanner(bytes.NewReader(content))
+		for scanner.Scan() {
+			line := strings.TrimSpace(scanner.Text())
 
-func executeSteps(reader io.ReadCloser) (result int) {
-	defer func() { _ = reader.Close() }()
+			if line == "" || strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
+				continue
+			}
 
-	scanner := bufio.NewScanner(reader)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
+			fmt.Print("\n$ ", line, "    # Execute? [Y/n] ")
+			if !yes() {
+				log.Println("Skipping step...")
+				continue
+			}
 
-		if line == "" || strings.HasPrefix(line, "#") || strings.HasPrefix(line, "//") {
-			continue
+			log.Println()
+
+			shell(line)
+
+			commandsExecuted++
 		}
-
-		fmt.Print("\n$ ", line, "    # Execute? [Y/n] ")
-		if !yes() {
-			log.Println("Skipping step...")
-			continue
-		}
-
-		log.Println()
-
-		shell(line)
-
-		result++
 	}
-	err := scanner.Err()
-	if err != nil {
-		log.Fatalln("scanner err:", err)
-	}
-	return result
+
+	log.Printf("Finished running %d commands.", commandsExecuted)
 }
 
 func shell(line string) {
